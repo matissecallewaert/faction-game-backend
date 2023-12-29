4992715bf4a13094890717ea99401c043c4736f8
Matisse Callewaert <matisse.callewaert@gmail.com>
2023-12-29 22:26:11 +0100

    :closed_lock_with_key: Update config file

--- src/Config.ts
+++ src/Config.ts
@@ -7,27 +7,7 @@ function getConfigVar(key: string): string {
 }
 
 export class Config {
-  static getAmpqUrl(): string {
-    return getConfigVar("URL_PRODUCTION_QUEUE");
-  }
-
-  static getNotifyApiQueueName(): string {
-    return getConfigVar("NOTIFY_API_SERVICE_QUEUE_NAME");
-  }
-
-  static getNotifyIngestDoneQueueName(): string {
-    return getConfigVar("NOTIFY_INGEST_DONE_QUEUE_NAME");
-  }
-
-  static getNotifyConnectorQueueName(): string {
-    return getConfigVar("NOTIFY_CONNECTOR_QUEUE_NAME");
-  }
-
-  static getNotifyNewInvoicesQueueName(): string {
-    return getConfigVar("NOTIFY_NEW_INVOICES_QUEUE_NAME");
-  }
-
-  static getNotifyIngestDoneFailedQueueName(): string {
-    return getConfigVar("NOTIFY_INGEST_DONE_FAILED_QUEUE_NAME");
+  static getDatabaseUrl(): string {
+    return getConfigVar("URL_DATABASE");
   }
 }
