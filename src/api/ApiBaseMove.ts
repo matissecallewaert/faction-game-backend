bf15bb7fb5571b96caa651e23d0ef17fedeadb9a
Matisse Callewaert <matisse.callewaert@gmail.com>
2024-01-03 20:44:08 +0100

    :alien: Update api

--- src/api/ApiBaseMove.ts
+++ src/api/ApiBaseMove.ts
@@ -1,17 +1,29 @@
 import { BASEMOVETYPE } from "../constants/BaseMoveType";
 import { FactionContext } from "../objects/FactionContext";
 import { GameContext } from "../objects/GameContext";
+import { UNITTYPE } from "../prisma";
 import ApiInstance from "./ApiInstance";
 
+export type PostBaseMoveResponse = {
+  baseMoveType: BASEMOVETYPE;
+  unitType?: UNITTYPE;
+};
+
 class BaseMoveApi {
-  public async PostBaseMove(faction_url: string, gameContext: GameContext, FactionContext: FactionContext): Promise<BASEMOVETYPE> {
+  public async PostBaseMove(
+    faction_url: string,
+    gameContext: GameContext,
+    FactionContext: FactionContext
+  ): Promise<PostBaseMoveResponse> {
     // const response = await ApiInstance.getApi(faction_url).post("/basemove", {
     //     gameContext: gameContext,
     //     factionContext: FactionContext,
     // });
     // return response.data.data;
 
-    return BASEMOVETYPE.IDLE;
+    return {
+      baseMoveType: BASEMOVETYPE.IDLE,
+    };
   }
 }
 
