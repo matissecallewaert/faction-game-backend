e69e3736717661890ea0a35f15f1b50af37058c4
Matisse Callewaert <matisse.callewaert@gmail.com>
2024-01-06 21:05:38 +0100

    :alien: Add unit api

---
+++ src/api/ApiUnitMove.ts
@@ -0,0 +1,30 @@
+import { UNITMOVETYPE } from "../constants/UnitMoveType";
+import { FactionContext } from "../objects/FactionContext";
+import { GameContext } from "../objects/GameContext";
+import { UnitContext } from "../objects/Unit";
+import ApiInstance from "./ApiInstance";
+
+export type PostUnitMoveResponse = {
+  unitMoveType: UNITMOVETYPE;
+  location?: Location;
+};
+
+class BaseUnitApi {
+  public async PostUnitMove(
+    faction_url: string,
+    gameContext: GameContext,
+    factionContext: FactionContext,
+    unitContext: UnitContext
+  ): Promise<PostUnitMoveResponse> {
+    // const response = await ApiInstance.getApi(faction_url).post("/unitmove", {
+    //     gameContext: gameContext,
+    //     factionContext: FactionContext,
+    //     unitContext: unitContext,
+    // });
+    // return response.data.data;
+
+    return { unitMoveType: UNITMOVETYPE.IDLE };
+  }
+}
+
+export default new BaseUnitApi();
